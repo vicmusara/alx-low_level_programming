878e9ac99a20e715874766958175b56db5182a03
vicmusara <saungweme.one@gmail.com>
2023-10-05 01:13:59 -0700

    Still has some betty errors  im tired. will finish later

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * puts not allowed
+ *
+ * return for success
+ */
+int main(void)
+{
+    printf("...with proper grammar, but the outcome is a piece of art,\n");
+    return 0;
+}
+
