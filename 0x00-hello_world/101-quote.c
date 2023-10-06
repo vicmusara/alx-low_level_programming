863c0f1156c5d9e77790ac540d1c2d5e256dd83f
vicmusara <saungweme.one@gmail.com>
2023-10-05 23:30:54 -0700

    corrected

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,17 @@
 #include <unistd.h>
 #include <stdio.h>
 /*
- * This program uses the write function 
- *
- * from the unistd.h library
- */
+* This program uses the write function 
+*
+* from the unistd.h library
+*/
 int main(void) 
 {
-    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    ssize_t len = 52;
-
-    if (write(2, message, len) != len) {
-        return 1;
-    }
-
-    return 1;
+const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+ssize_t len = 52;
+if (write(2, message, len) != len) 
+{
+ return 1;
+}
+return 1;
 }
